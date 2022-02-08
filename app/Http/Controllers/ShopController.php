1939157c656fc96cdb7302555bf3470747c0ca13
yapiko <bloomingjewels7021@gmail.com>
2022-02-08 14:56:11 +0900

    cooks-laravel4

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -33,6 +33,30 @@ class ShopController extends Controller
         return redirect()->route('shop.detail', ['id' => $shop->id]);
     }
 
+    public function edit(Shop $shop, $id)
+    {
+        $shop = Shop::find($id);
+        $categories = Category::all()->pluck('name', 'id');
+        return view('edit', ['shop' => $shop, 'categories' => $categories]);
+    }
+
+    public function update(Request $request, $id, Shop $shop)
+    {
+        $shop = Shop::find($id);
+        $shop->name = request('name');
+        $shop->address = request('address');
+        $shop->category_id = request('category_id');
+        $shop->save();
+        return redirect()->route('shop.detail', ['id' => $shop->id]);
+    }
+
+    public function destory($id)
+    {
+        $shop = Shop::find($id);
+        $shop->delete();
+        return redirect('/shops');
+    }
+
     public function show($id)
     {
         $shop = Shop::find($id);
