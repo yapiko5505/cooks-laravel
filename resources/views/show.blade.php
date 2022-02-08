19ffc70f5950102ad02e5730a6345f26c1070376
yapiko <bloomingjewels7021@gmail.com>
2022-02-08 14:56:34 +0900

    cooks-laravel5

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -7,5 +7,10 @@
     </div>   
     <div>
         <a href={{ route('shop.list') }}>一覧に戻る</a>
+        <a href={{ route('shop.edit', ['id' => $shop->id]) }}>編集</a>
+        <p></p>
+        {{ Form::open(['method' => 'delete', 'route' => ['shop.destory', $shop->id]]) }}
+            {{ Form::submit('削除')}}
+        {{ Form::close() }}
     </div>    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/shops', 'App\Http\Controllers\ShopController@index')->name('shop.list');
 Route::get('/shop/new', 'App\Http\Controllers\ShopController@create')->name('shop.new');
 Route::post('/shop', 'App\Http\Controllers\ShopController@store')->name('shop.store');
+Route::get('/shop/edit/{id}', 'App\Http\Controllers\ShopController@edit')->name('shop.edit');
+Route::post('/shop/update/{id}', 'App\Http\Controllers\ShopController@update')->name('shop.update');
 
 Route::get('/shop/{id}', 'App\Http\Controllers\ShopController@show')->name('shop.detail');
+Route::delete('/shop/{id}', 'App\Http\Controllers\ShopController@destory')->name('shop.destory');
 
 Route::get('/', function () {
     // return view('welcome');
