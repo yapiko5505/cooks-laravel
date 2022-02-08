3acf68938cedbb37920104cb7d9e12ddbe1b24f3
yapiko <bloomingjewels7021@gmail.com>
2022-02-08 18:27:06 +0900

    cooks-laravel6

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,7 +4,13 @@
     <div>
         <p>{{ $shop->category->name }}</p>
         <p>{{ $shop->address }}</p>
-    </div>   
+    </div>  
+        <iframe id='map'
+            src='https://www.google.com/maps/embed/v1/place?key=AIzaSyB_k4UbdjDG_IRjAMSDm-7mDSfHCGCxpUY&q={{ $shop->address }}'
+            width='100%'
+            height='320'
+            frameborder='0'>
+        </iframe> 
     <div>
         <a href={{ route('shop.list') }}>一覧に戻る</a>
         <a href={{ route('shop.edit', ['id' => $shop->id]) }}>編集</a>
